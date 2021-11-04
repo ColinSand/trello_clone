5926163ec863bc84038f756ef042e2bd5a159b09
Colin Sand <Ottermij2@gmail.com>
2021-11-04 12:21:05 -0500

    Connecting trello list to to lists and cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import TrelloList from "./TrelloList";
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
+    const { lists } = this.props;
     return (
       <div className="App">
         <h2>Does this render?</h2>
+        {lists.map((list) => (
+          <TrelloList title={list.title} cards={list.cards} />
+        ))}
         <TrelloList title="test" />
       </div>
     );
   }
 }
-export default App;
+const mapStateToProps = (state) => ({
+  lists: state.lists,
+});
+export default connect(mapStateToProps)(App);
