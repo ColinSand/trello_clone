75039793538f182570d1e6a2cc3a5044cde06dd0
Colin Sand <Ottermij2@gmail.com>
2021-11-05 10:09:50 -0500

    Adds the react beautiful dnd package

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,46 @@ import React, { Component } from "react";
 import TrelloList from "./TrelloList";
 import { connect } from "react-redux";
 import TrelloActionButton from "./TrelloActionButton";
+import { DragDropContext } from "react-beautiful-dnd";
 
 class App extends Component {
+  onDragEnd = () => {};
   render() {
     const { lists } = this.props;
     return (
-      <div className="App">
-        <h2>Trello Clone</h2>
-        <div style={styles.listsContainer}>
-          {/* maps out each key on the TrelloList component */}
-          {lists.map((list) => (
-            <TrelloList
-              listID={list.id}
-              key={list.id}
-              title={list.title}
-              cards={list.cards}
-            />
-          ))}
-          <TrelloActionButton list></TrelloActionButton>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="grandTetonBackground">
+          <h2>Trello Clone</h2>
+
+          <div style={styles.listsContainer}>
+            {/* maps out each key on the TrelloList component */}
+            {lists.map((list) => (
+              <TrelloList
+                listID={list.id}
+                key={list.id}
+                title={list.title}
+                cards={list.cards}
+              />
+            ))}
+            <TrelloActionButton list></TrelloActionButton>
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     );
   }
 }
 const styles = {
+  // background: {
+  //   backgroundImage: `url(${grandTeton})`,
+  //   backgroundSize: cover,
+  //   backgroundAttachment: fixed,
+  //   backgroundBlendMode: screen,
+  //   backgroundColor: green,
+  //   fontFamily: Arial,
+  //   Helvetica,
+  //   sansSerif,
+  //   paddingBottom: 100,
+  // },
   listsContainer: {
     display: "flex",
     flexDirection: "row ",
--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -2,16 +2,28 @@ import React from "react";
 import TrelloCard from "./TrelloCard";
 import TrelloActionButton from "./TrelloActionButton";
 import listsReducer from "../reducers/listsReducer";
+import { Droppable } from "react-beautiful-dnd";
 
 const TrelloList = ({ title, cards, listID }) => {
   return (
-    <div style={styles.container}>
-      <h4>{title}</h4>
-      {cards.map((card) => (
-        <TrelloCard key={card.id} text={card.text} />
-      ))}
-      <TrelloActionButton listID={listID} />
-    </div>
+    <Droppable droppableId={String(listID)}>
+      {/* to get Droppable to work the following was wrapped in the "provided" function */}
+      {(provided) => (
+        <div
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          style={styles.container}
+        >
+          <h4>{title}</h4>
+          {cards.map((card) => (
+            // the card.id here is for later reference
+            <TrelloCard id={card.id} key={card.id} text={card.text} />
+          ))}
+          <TrelloActionButton listID={listID} />
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 };
 
