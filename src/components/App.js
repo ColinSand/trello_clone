a33fad5799b44bb0c655b5a3d14e8911a73a45a3
Colin Sand <Ottermij2@gmail.com>
2021-12-09 13:22:10 -0600

    Fixes white space on adding lists

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { sort } from "../actions";
 import styled from "styled-components";
 import grandTeton from "../images/grandTeton.jpeg";
+import componentCSS from "./Components.css";
 
 // these styled components create custom HTML components with their own styling
 const ListContainer = styled.div`
@@ -13,23 +14,8 @@ const ListContainer = styled.div`
   height: 100%;
 `;
 
-// 12/6/21 note: possibly need to use the scroll function to just move around the lists and not the whole page
-const MainBackground = styled.div`
-  background-image: url(${grandTeton});
-  background-size: cover;
-  background-attachment: fixed;
-  background-blend-mode: screen;
-  font-family: Arial, Helvetica, sans-serif;
-  padding-bottom: 100px;
-  min-height: 800px;
-  width: 100vw;
-  // min-width: 2000px;
-  margin: -25px -10px -10px -8px;
-`;
-
 const PageTitle = styled.h1`
   color: white;
-  text-align: center;
   padding-top: 45px;
   padding-bottom: 25px;
 `;
@@ -58,31 +44,26 @@ class App extends Component {
         onDragEnd={this.onDragEnd}
         className="grandTetonBackground"
       >
-        <MainBackground>
-          <PageTitle>Colin Sand's Trello Clone</PageTitle>
-          {/* the droppableId can essentially be anything here */}
-          <Droppable droppableId="all-lists" direction="horizontal" type="list">
-            {(provided) => (
-              <ListContainer
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {/* maps out each key on the TrelloList component */}
-                {lists.map((list, index) => (
-                  <TrelloList
-                    listID={list.id}
-                    key={list.id}
-                    title={list.title}
-                    cards={list.cards}
-                    index={index}
-                  />
-                ))}
-                {provided.placeholder}
-                <TrelloActionButton list />
-              </ListContainer>
-            )}
-          </Droppable>
-        </MainBackground>
+        <PageTitle>Colin Sand's Trello Clone</PageTitle>
+        {/* the droppableId can essentially be anything here */}
+        <Droppable droppableId="all-lists" direction="horizontal" type="list">
+          {(provided) => (
+            <ListContainer {...provided.droppableProps} ref={provided.innerRef}>
+              {/* maps out each key on the TrelloList component */}
+              {lists.map((list, index) => (
+                <TrelloList
+                  listID={list.id}
+                  key={list.id}
+                  title={list.title}
+                  cards={list.cards}
+                  index={index}
+                />
+              ))}
+              {provided.placeholder}
+              <TrelloActionButton list />
+            </ListContainer>
+          )}
+        </Droppable>
       </DragDropContext>
     );
   }
